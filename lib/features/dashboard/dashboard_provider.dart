7d8d06d85fa61f881cc875409bb64d4d554a8ce1
aka-FinGo <akafingo@gmail.com>
2026-03-06 18:50:19 +0500

    Update dashboard_provider.dart

--- lib/features/dashboard/dashboard_provider.dart
+++ lib/features/dashboard/dashboard_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
+import 'package:fl_chart/fl_chart.dart';
 // --------------------------------------------------------
 // 1. FILTRLAR (Vaqt va Xodim)
 // --------------------------------------------------------
