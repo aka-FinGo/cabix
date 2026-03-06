b3d53843daa63f9b8b18207b3837fd05012136fe
aka-FinGo <akafingo@gmail.com>
2026-03-06 20:41:13 +0500

    Update dashboard_screen.dart

--- lib/features/dashboard/dashboard_screen.dart
+++ lib/features/dashboard/dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import '../../core/theme/theme_provider.dart';
 import 'dashboard_provider.dart';
 import 'dashboard_widgets.dart';
 
